de918abe8e865b2a509b03d16227c274760b672d
Jerry <usyuuryou@163.com>
2024-01-14 15:33:52 +0900

    up

--- src/components/Dashboard/02/workcircle/index.tsx
+++ src/components/Dashboard/02/workcircle/index.tsx
@@ -5,39 +5,39 @@ import Title from "../title";
 import ReactEcharts from "echarts-for-react"
 
 const WorkCircle: React.FC = () => {
-    const option = {
-        title: {
-            text: '',
-            subtext: '',
-            left: 'center'
-        },
-        tooltip: {
-            trigger: 'item',
-            formatter: '{a} <br/>{b} : {c} ({d}%)'
-        },
-        series: [
-            {
-                type: 'pie',
-                radius: '95%',
-                center: ['50%', '50%'],
-                selectedMode: 'single',
-                data: [
-                    { value: 435, name: 'A' },
-                    { value: 735, name: 'B' },
-                    { value: 510, name: 'C' },
-                    { value: 434, name: 'D' },
-                    { value: 335, name: 'E' }
-                ],
-                emphasis: {
-                    itemStyle: {
-                        shadowBlur: 10,
-                        shadowOffsetX: 0,
-                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+    const
+        option = {
+            title: {
+                left: 'center'
+            },
+            tooltip: {
+                show: false,
+                trigger: 'item',
+                formatter: '{a} <br/>{b} : {c} ({d}%)'
+            },
+            series: [
+                {
+                    type: 'pie',
+                    radius: ['49%', "85%"],
+                    center: ['50%', '50%'],
+                    selectedMode: 'single',
+                    data: [
+                        { value: 735, name: 'A' },
+                        { value: 510, name: 'B' },
+                        { value: 434, name: 'C' },
+                        { value: 335, name: 'D' }
+                    ],
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
                     }
                 }
-            }
-        ]
-    };
+            ]
+        };
+
     return (
         <Container>
             <Title>
@@ -45,10 +45,9 @@ const WorkCircle: React.FC = () => {
             </Title>
             <ContainerCircle>
                 <ReactEcharts option={option} />
-                <div style={{position: "absolute", top: "0"}}>
+                <ClockBlock>
                     <MyClock />
-                </div>
-                
+                </ClockBlock>
             </ContainerCircle>
         </Container>
     )
@@ -61,13 +60,15 @@ const Container = styled.div`
 `
 
 const ContainerCircle = styled.div`
-    position: "relative";
+    position: relative;
     margin: 0 auto;
     padding-top: 20px;
 `
 
 const ClockBlock = styled.div`
-    
+    position: absolute;
+    top:30%;
+    left:33%
 `
 
 export default WorkCircle;
