6b4b54520c9b21fea2400ebc18c05c31b1c431b5
Jerry <usyuuryou@163.com>
2024-01-22 16:20:50 +0900

    up

--- src/components/Mamory/index.tsx
+++ src/components/Mamory/index.tsx
@@ -3,18 +3,18 @@ import axios from "axios";
 
 const Mamory: React.FC = () => {
     const { mamoryLogin } = window;
-    useEffect(() => {
-        mamoryLogin()
-            .then(data => console.log(data))
-            .catch(error => console.log(error))
-    }, [])
     // useEffect(() => {
-    //     axios.post("https://external-api.mamory.jp/v1/auth/token", {
-    //         "user_id": "exlnk-eJym4WXfkzLm",
-    //         "password": "UD&hNcC(g5-m"
-    //     }).then(data => console.log(data))
-    //         .catch(err => console.log(err))
+    //     mamoryLogin()
+    //         .then(data => console.log(data))
+    //         .catch(error => console.log(error))
     // }, [])
+    useEffect(() => {
+        axios.post("https://external-api.mamory.jp/v1/auth/token", {
+            "user_id": "exlnk-eJym4WXfkzLm",
+            "password": "UD&hNcC(g5-m"
+        }).then(data => console.log(data))
+            .catch(err => console.log(err))
+    }, [])
     return (
         <>
             <iframe id="frame1" width="400" height="300"></iframe>
