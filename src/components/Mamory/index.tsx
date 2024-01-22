845e726d2b12b927f607e151773f3a552a14346a
Jerry <usyuuryou@163.com>
2024-01-22 14:48:19 +0900

    up

--- src/components/Mamory/index.tsx
+++ src/components/Mamory/index.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect } from "react";
 
-
 const Mamory: React.FC = () => {
+    const { mamoryLogin } = window;
     useEffect(() => {
-        const head = document.getElementsByTagName('head')[0] as HTMLElement;
-        const scriptUrl = document.createElement('script');
-        scriptUrl.type = 'text/javascript';
-        scriptUrl.src = 'https://relay.mamory.jp/js/mamory-cameraview.js';
-        head.appendChild(scriptUrl);
-    }, []);
+        mamoryLogin()
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
+    }, [])
     return (
         <>
+            <iframe id="frame1" width="400" height="300"></iframe>
         </>
     )
 }
