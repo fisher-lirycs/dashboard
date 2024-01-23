2988e6e919559b7dca6458a88d94073ac5e223a7
Jerry <usyuuryou@163.com>
2024-01-23 18:37:00 +0900

    up

--- src/components/Dashboard/02/weather/index.tsx
+++ src/components/Dashboard/02/weather/index.tsx
@@ -11,38 +11,17 @@ const Weather: React.FC = () => {
             <TimeBlock>
                 <span>{today.getFullYear()}年　{today.getMonth() + 1}月{today.getDate()}日（{getWeek(today.getDay())}）</span>
             </TimeBlock>
-            <DateLine height="40%" style={{ marginBottom: "2%" }}>
-                <div style={{ float: "left", width: "65%", height: "100%" }}>
-                    <DateLine height="49%" style={{ marginBottom: "2%" }}>
-                        <Screen size="large" item="平均風速" value="0.7" unit="m/sec" />
-                    </DateLine>
-                    <DateLine height="49%">
-                        <Screen size="large" item="瞬間風速" value="0.1" unit="m/sec" />
-                    </DateLine>
-                </div>
-                <div style={{ float: "right", width: "31%", height: "100%" }}>
-                    <DateLine height="100%" >
-                        <Compass direction="SE" />
-                    </DateLine>
-                </div>
-            </DateLine>
-            <DateLine height="40%">
-                <DateLine height="49%" style={{ display: "flex", marginBottom: "1%" }}>
-                    <Screen item="温度" value="30.4" unit="℃" />
-                    <Screen item="湿度" value="60.0" unit="℃" />
-                    <Screen item="暑き指数" value="26℃/警戒" unit="" />
-                </DateLine>
-                <DateLine height="49%" style={{ display: "flex" }}>
-                    <Screen item="1時間雨量" value="0.0" unit="mm" />
-                    <Screen item="24時間雨量" value="0.0" unit="mm" />
-                    <Screen item="降り始め雨量" value="0.0" unit="mm" />
-                </DateLine>
+            <DateLine height="20%" style={{ display: "flex" }}>
+                <Screen item="温度" value="30.4" unit="℃" />
+                <Screen item="湿度" value="60.0" unit="℃" />
+                <Screen item="暑き指数" value="26℃/警戒" unit="" />
             </DateLine>
         </Container>
     )
 }
 
 const Container = styled.div`
+    position: relative;
     width: 98%;
     height: 80%;
     background-color: #000000;
@@ -57,6 +36,8 @@ const TimeBlock = styled.div`
 `
 
 const DateLine = styled.div<{ height: string, width?: string }>`
+    position: relative;
+    top: 70%;
     width:  ${props => props.width || "100%"};
     height: ${props => props.height};
 `
