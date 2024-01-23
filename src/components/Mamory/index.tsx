d9144f42240cbc5680e868ce862f39be55711678
Jerry <usyuuryou@163.com>
2024-01-23 20:08:00 +0900

    api

--- src/components/Mamory/index.tsx
+++ src/components/Mamory/index.tsx
@@ -2,13 +2,14 @@ import React, { useEffect } from "react";
 import axios from "axios";
 
 const Mamory: React.FC = () => {
+    const { getCameraUrl } = window;
     useEffect(() => {
-        axios.post("https://external-api.mamory.jp/v1/auth/token", {
-            "user_id": "exlnk-eJym4WXfkzLm",
-            "password": "UD&hNcC(g5-m"
-        }).then(data => console.log(data))
-            .catch(err => console.log(err))
-    }, [])
+        getCameraUrl("exlnk-eJym4WXfkzLm", "UD&hNcC(g5-m", "1503-5254-4767-2882").then(data => {
+            console.log(data);
+        }).catch(error => {
+            console.log(error);
+        })
+    }, [getCameraUrl])
     return (
         <>
             <iframe id="frame1" width="400" height="300"></iframe>
