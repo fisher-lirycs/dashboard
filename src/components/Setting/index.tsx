3c11ac8efc2e06edd742396c108e713811b98849
Jerry <usyuuryou@163.com>
2024-01-23 16:37:02 +0900

    up

--- src/components/Setting/index.tsx
+++ src/components/Setting/index.tsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { HtmlHTMLAttributes, useCallback, useState } from "react";
 import styled from "styled-components";
 
 const Setting: React.FC = () => {
+    const [displayFileName, setDisplayFileName] = useState<string>("選択されていません");
+    const handleFileSelect = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = null || e.target.files && e.target.files[0]
+        const name = selectedFile?.name || "選択されていません";
+        setDisplayFileName(name)
+    }, [])
+
+
     return (
         <Contanier>
             <Content>
+                <FileUpBlock style={{ marginBottom: "5px" }}>
+                    <Lable htmlFor="file">日程ファイル</Lable>
+                    <span>{displayFileName}</span>
+                    <FileInput>
+                        <span>選択</span>
+                        <input type="file" name="file" id="file" onChange={handleFileSelect} />
+                    </FileInput>
+                </FileUpBlock>
                 <div>
-                    <FileInput type="file" name="file" id="file" />
-                </div>
-                <div>
-                    <Lable htmlFor="file">スライド</Lable>
+                    <Lable htmlFor="file">スライドの切替</Lable>
                     <NummberInput type="number" name="slideTime" id="slideTime" />
                     <span>秒</span>
                 </div>
-                <div>
-                    <input type="button" value="設定" />
-                </div>
+                <ButtonBlock>
+                    <Button type="button" value="設定" />
+                </ButtonBlock>
             </Content>
         </Contanier>
     )
@@ -31,7 +44,7 @@ const Contanier = styled.div`
 
 const Content = styled.div`
     width: 300px;
-    height: 300px;
+    height: 150px;
     padding: 30px;
     background: #fff;
     box-shadow: 0 0 8px rgba(0,0,0,.12);
@@ -45,7 +58,7 @@ const Lable = styled.label`
     margin-right: .75rem;
     height: 40px;
     width: 100px;
-    padding-left: 10px;
+    padding: 0 5px;
     background: #eaeaea;
     font-weight: 700;
     font-size: 14px;
@@ -54,7 +67,7 @@ const Lable = styled.label`
 `
 
 const NummberInput = styled.input`
-    width: 124px!important;
+    width: 124px;
     display: inline-flex;
     align-items: center;
     margin-right: .75rem;
@@ -71,21 +84,67 @@ const NummberInput = styled.input`
     transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
 `
 
-const FileInput = styled.input`
-    cursor: pointer;
+const FileUpBlock = styled.div`
+    position: relative;
+    display: flex;
+    width: 100%;
+    align-items: center;
+    font-size: 13px;
+`
+const FileInput = styled.div`
+    position: absolute;
+    display: flex;
+    right: 0;
     overflow: hidden;
-    appearance: none;
-    background-clip: padding-box;
-    background-color: #212529;
-    border: 1px solid #495057;
+    width: 30px;
+    height: 20px;
+    border: 1px solid #eaeaea;
+    border-radius: 4px;
+    align-items: center;
+    justify-content: center;
+    background: #eaeaea;
+    cursor: pointer;
+    font-size: 12px;
+
+    & > input {
+        display: inline-block;
+        position: absolute;
+        font-size: 12px;
+        top: 0;
+        left: 0;
+        opacity: 0;
+        z-index: 1;
+        cursor: pointer;
+    }
+`
+
+const ButtonBlock = styled.div`
+    position: relative;
+    display: flex;
+    width: 100%;
+    margin-top: 20px;
+    align-items: center;
+    justify-content: center;
+    font-size: 13px;
+`
+
+const Button = styled.input`
+    display: inline-block;
+    width: 100px;
+    background-color: #198754;
+    border: 1px solid #198754;
     border-radius: 0.375rem;
-    color: #495057;
-    display: block;
     font-size: 1rem;
     font-weight: 400;
     line-height: 1.5;
+    color: #fff;
     padding: 0.375rem 0.75rem;
-    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-    width: 100%;
+    text-align: center;
+    text-decoration: none;
+    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    user-select: none;
+    vertical-align: middle;
+    cursor: pointer;
 `
+
 export default Setting;
