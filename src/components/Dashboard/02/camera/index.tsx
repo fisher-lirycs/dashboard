d7d326bc4a55a6f5a57af7628eaab69637f0df5d
Jerry <usyuuryou@163.com>
2024-01-24 16:02:49 +0900

    up

--- src/components/Dashboard/02/camera/index.tsx
+++ src/components/Dashboard/02/camera/index.tsx
@@ -6,11 +6,12 @@ const Carama: React.FC = () => {
     const { getCameraUrl } = window;
     const [cameraUrl, setCameraUrl] = useState("");
     const serialId = "1503-5254-4767-2882";
+    const [controlToken, setControlToken] = useState("");
 
     useEffect(() => {
         getCameraUrl("exlnk-eJym4WXfkzLm", "UD&hNcC(g5-m", serialId).then(data => {
             let url = data.url;
-            let qs = "aspect_ratio=4_3&";
+            let qs = "aspect_ratio=16_9&";
             qs += "pause_resume_enable=enable";
             setCameraUrl(url + "?" + qs)
         }).then(() => {
@@ -28,28 +29,36 @@ const Carama: React.FC = () => {
                 "user_id": "exlnk-eJym4WXfkzLm",
                 "password": "UD&hNcC(g5-m"
             }
-        }).then((data) => {
-            console.log(data);
+        }).then(({ data }) => {
+            const access_token = data.access_token;
+            setControlToken(access_token);
         }).catch(error => {
             console.log(error);
 
         })
-        // axios({
-        //     url: `https://external-api.mamory.jp/v1/camera_controls/${serialId}/brightness`,
-        //     method: 'put',
-        //     data: {
-        //         "brightness": 1
-        //     },
-        //     headers: {
-        //         Authorization: `Bearer ${localStorage.getItem("cameraToken")}`
-        //     }
-        // }).then((data) => {
-        //     console.log(data);
-        // }).catch(error => {
-        //     console.log(error);
 
-        // })
     }, [])
+
+    useEffect(() => {
+        if (controlToken) {
+            axios({
+                url: `https://external-api.mamory.jp/v1/camera_controls/${serialId}/brightness`,
+                method: 'put',
+                data: {
+                    "brightness": 1
+                },
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("cameraToken")}`
+                }
+            }).then((data) => {
+                console.log("success");
+                console.log(data);
+            }).catch(error => {
+                console.log(error);
+
+            })
+        }
+    }, [controlToken])
 
 
     return <CameraIframe id="cameraIframe" src={cameraUrl} />
