788ef8b5f0df6374cee499a7ae15d212c31ed7b2
Jerry <usyuuryou@163.com>
2024-01-24 10:18:54 +0900

    up

--- src/components/Dashboard/02/reservation/index.tsx
+++ src/components/Dashboard/02/reservation/index.tsx
@@ -6,8 +6,10 @@ import { ScheduleDataType } from "../../../../types/Types";
 const Reservation: React.FC = () => {
     const [schedules, setSchedules] = useState<Array<ScheduleDataType>>();
     useEffect(() => {
-        const scheduleData: Array<ScheduleDataType> = JSON.parse(localStorage.getItem("schedule") || "");
-        setSchedules(scheduleData);
+        if (localStorage.getItem("schedule")) {
+            const scheduleData: Array<ScheduleDataType> = JSON.parse(localStorage.getItem("schedule") || "");
+            setSchedules(scheduleData);
+        }
     }, [])
     return (
         <Container>
