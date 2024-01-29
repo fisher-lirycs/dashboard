f034b09fc3a653dcbe4b4bbb6e9f79cb4d588867
Jerry <usyuuryou@163.com>
2024-01-29 10:56:22 +0900

    up

--- src/components/Dashboard/02/weather/index.tsx
+++ src/components/Dashboard/02/weather/index.tsx
@@ -103,14 +103,14 @@ const MiddleDateContent = styled.div`
 
 const CameraBlock = styled.div`
     height: 100%;
-    width: 56.5%;
+    width: 65%;
     margin-left: 1%;
 `
 
 
 const MiddleWeatherBlock = styled.div`
     height: 100%;
-    width: 41%;
+    width: 33%;
     margin-left: 1%;
 `
 
