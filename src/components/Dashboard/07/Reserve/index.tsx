c940738d76e896253390739e7a32864604a207c9
usyuuryou <usyuuryou@163.com>
2024-02-06 20:20:58 +0800

    up

--- src/components/Dashboard/07/Reserve/index.tsx
+++ src/components/Dashboard/07/Reserve/index.tsx
@@ -1,36 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import Title from "../Title";
 import Schedule from "./Schedule";
 
 const Reserve: React.FC = () => {
-    return (
-        <CircleContainer>
-            <TitleContent>
-                <Title text={"今週の作業予定"} />
-            </TitleContent>
-            <ScheduleContent>
-                <Schedule />
-            </ScheduleContent>
-        </CircleContainer>
-    )
-}
+  return (
+    <CircleContainer>
+      <TitleContent>
+        <Title text={"今週の作業予定"} />
+      </TitleContent>
+      <ScheduleContent>
+        <Schedule />
+      </ScheduleContent>
+    </CircleContainer>
+  );
+};
 
 const CircleContainer = styled.div`
-    width: 100%;
-    height: 100%;
-`
+  width: 100%;
+  height: 100%;
+`;
 const TitleContent = styled.div`
-    width: 100%;
-    height: 25px;
-`
+  width: 100%;
+  height: 25px;
+`;
 
 const ScheduleContent = styled.div`
-    display: flex;
-    width: 100%;
-    height: calc(100% - 25px);
-    align-items: center;
-    justify-content: center;
-`
+  display: flex;
+  width: 100%;
+  height: calc(100% - 25px);
+  align-items: center;
+  justify-content: center;
+`;
 
 export default Reserve;
