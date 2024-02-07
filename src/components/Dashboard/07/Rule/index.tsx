7b9c7c9e309fa65e3805d057efc8e67530c7b22d
unknown <yuqiuliang@links.sz>
2024-02-07 14:31:27 +0800

    up

--- src/components/Dashboard/07/Rule/index.tsx
+++ src/components/Dashboard/07/Rule/index.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Title from "../Title";
+import { ReactComponent as ColseImage } from "./../../../../assets/images/close_white.svg";
+import { ReactComponent as SetImage } from "./../../../../assets/images/setting.svg";
 
 const Rule: React.FC = () => {
+    const [editStatus, setEditStatus] = useState(false)
+    const [ruleDetail, setRuleDetail] = useState((localStorage.getItem("ruleDetail")) || "")
+
+    const handleBlur = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const target = e.target;
+        const value = target.value;
+        setRuleDetail(value)
+        localStorage.setItem("ruleDetail", value);
+    }
+
     return (
         <RuleContainer>
+            <EditIconBlock>
+                {editStatus && <ColseImage width={"100%"} height={"100%"} onClick={() => setEditStatus(!editStatus)} />}
+                {!editStatus && <SetImage width={"100%"} height={"100%"} onClick={() => setEditStatus(!editStatus)} />}
+            </EditIconBlock>
             <TitleContent>
                 <Title text={"ルール"} />
             </TitleContent>
-            <DetailContent></DetailContent>
+            {editStatus ? (
+                <InputContent>
+                    <textarea defaultValue={ruleDetail} rows={6} onBlur={(e) => { handleBlur(e) }} />
+                </InputContent>
+            ) : (
+                <DetailContent>
+                    {ruleDetail}
+                </DetailContent>
+            )}
+
         </RuleContainer>
     )
 }
@@ -29,6 +54,37 @@ const DetailContent = styled.div`
     margin-top: 5px;
     padding: 5px;
     background: linear-gradient(#5acdef, #dff8ff);
+    font-size: 1vw;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+    word-break: normal;
+`
+
+const InputContent = styled.div`
+    margin-top: 10px;
+    font-weith: bold;
+
+    & textarea {
+        width: calc(100% - 10px);
+        height: 100%;
+        font-size: 1vw;
+        font-weight: 400;
+        line-height: 1.5;
+        color: #495057;
+        background-color: #fff;
+        background-clip: padding-box;
+        border: 1px solid #ced4da;
+        border-radius: 0.25rem;
+        transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    }
+`
+
+const EditIconBlock = styled.div`
+  position: absolute;
+  width: 25px;
+  height: 25px;
+  top: 0;
+  right: 0;
 `
 
 
--- src/components/Dashboard/07/Safety/index.tsx
+++ src/components/Dashboard/07/Safety/index.tsx
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Title from "../Title";
+import { ReactComponent as ColseImage } from "./../../../../assets/images/close_white.svg";
+import { ReactComponent as SetImage } from "./../../../../assets/images/setting.svg";
 
 export interface SafetyProps {
     fontSize?: string
 }
 
-const Safety: React.FC<SafetyProps> = ({ fontSize = "1.5vw" }) => {
+const Safety: React.FC<SafetyProps> = ({ fontSize = "1.2vw" }) => {
+    const [editStatus, setEditStatus] = useState(false)
+    const [safetyDetail, setSafeDetail] = useState((localStorage.getItem("safetyDetail")) || "私はルールを正しく守り、\r\n自分の身は自分で守ります")
+
+    const handleBlur = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const target = e.target;
+        const value = target.value;
+        setSafeDetail(value)
+        localStorage.setItem("safetyDetail", value);
+    }
+
     return (
         <SafetyContainer>
+            <EditIconBlock>
+                {editStatus && <ColseImage width={"100%"} height={"100%"} onClick={() => setEditStatus(!editStatus)} />}
+                {!editStatus && <SetImage width={"100%"} height={"100%"} onClick={() => setEditStatus(!editStatus)} />}
+            </EditIconBlock>
             <TitleContent>
                 <Title text={"安 全"} />
             </TitleContent>
-            <TextContent fontSize={fontSize}>
-                <span>私はルールを正しく守り、</span>
-                <br />
-                <span>自分の身は自分で守ります</span>
-            </TextContent>
+            {editStatus ? (
+                <InputContent fontSize={fontSize}>
+                    <textarea defaultValue={safetyDetail} rows={4} onBlur={(e) => { handleBlur(e) }} />
+                </InputContent>
+            ) : (
+                <TextContent fontSize={fontSize}>
+                    {safetyDetail}
+                </TextContent>
+            )}
+
         </SafetyContainer>
     )
 }
@@ -35,7 +56,37 @@ const TextContent = styled.div<{ fontSize: string }>`
     padding-left: 20px;
     margin-top: 20px;
     font-size: ${props => props.fontSize};
+    white-space: pre-wrap;
+    word-wrap: break-word;
+    word-break: normal;
+`
+
+const InputContent = styled.div<{ fontSize: string }>`
+    margin-top: 10px;
+    font-size: ${props => props.fontSize};
     font-weith: bold;
+
+    & textarea {
+        width: calc(100% - 10px);
+        height: 100%;
+        font-size: ${props => props.fontSize};
+        font-weight: 400;
+        line-height: 1.5;
+        color: #495057;
+        background-color: #fff;
+        background-clip: padding-box;
+        border: 1px solid #ced4da;
+        border-radius: 0.25rem;
+        transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    }
+`
+
+const EditIconBlock = styled.div`
+  position: absolute;
+  width: 25px;
+  height: 25px;
+  top: 0;
+  right: 0;
 `
 
 export default Safety;
