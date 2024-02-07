437ba184d8fdf1873d980f714399927d322799ee
unknown <yuqiuliang@links.sz>
2024-02-07 14:03:57 +0800

    pu

--- src/components/Dashboard/07/Reserve/Schedule/index.tsx
+++ src/components/Dashboard/07/Reserve/Schedule/index.tsx
@@ -1,67 +1,97 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styled from "styled-components";
 import { ScheduleDataType } from "../../../../../types/Types";
 
-const Schedule: React.FC = () => {
-  const [schedules, setSchedules] = useState<Array<ScheduleDataType>>();
-  useEffect(() => {
-    const defaultDate: Array<ScheduleDataType> = [
-      {
-        day: "1/22",
-        week: "月",
-        detail: "杭工事",
-      },
-      {
-        day: "1/23",
-        week: "火",
-        detail: "掘削工事",
-      },
-      {
-        day: "1/24",
-        week: "水",
-        detail: "地下躯体工事",
-      },
-      {
-        day: "1/25",
-        week: "木",
-        detail: "地上躯体工事",
-      },
-      {
-        day: "1/26",
-        week: "金",
-        detail: "外装工事",
-      },
-      {
-        day: "1/27",
-        week: "土",
-        detail: "内装工事",
-      },
-      {
-        day: "1/28",
-        week: "日",
-        detail: "外構工事",
-      },
-    ];
-    setSchedules(defaultDate); 
-  }, []);
+export interface ScheduleProps {
+  editStatus: boolean
+}
+const Schedule: React.FC<ScheduleProps> = ({ editStatus }) => {
+  const [schedules, setSchedules] = useState<Array<ScheduleDataType>>(JSON.parse(localStorage.getItem("scheduleDetail") as string) || [
+    {
+      day: "1/22",
+      week: "月",
+      detail: "杭工事",
+    },
+    {
+      day: "1/23",
+      week: "火",
+      detail: "掘削工事",
+    },
+    {
+      day: "1/24",
+      week: "水",
+      detail: "地下躯体工事",
+    },
+    {
+      day: "1/25",
+      week: "木",
+      detail: "地上躯体工事",
+    },
+    {
+      day: "1/26",
+      week: "金",
+      detail: "外装工事",
+    },
+    {
+      day: "1/27",
+      week: "土",
+      detail: "内装工事",
+    },
+    {
+      day: "1/28",
+      week: "日",
+      detail: "外構工事",
+    },
+  ]);
+
+  const handleBlur = (e: React.ChangeEvent<HTMLInputElement>, index: number, item: "day" | "week" | "detail") => {
+    const target = e.target;
+    const value = target.value;
+    let scheduleDataTemp = schedules;
+    scheduleDataTemp[index][item] = value;
+    localStorage.setItem("scheduleDetail", JSON.stringify(scheduleDataTemp));
+  }
 
   return (
     <Container>
-      <Table>
-        <tbody>
-          {schedules &&
-            schedules.length > 0 &&
-            schedules.map((schedule) => (
-              <tr key={schedule.day}>
-                <td className="day">{schedule.day}</td>
-                <TdWeek className="week" week={schedule.week}>
-                  {schedule.week}
-                </TdWeek>
-                <td className="detail">{schedule.detail}</td>
-              </tr>
-            ))}
-        </tbody>
-      </Table>
+      {!editStatus ? (
+        <Table>
+          <tbody>
+            {schedules &&
+              schedules.length > 0 &&
+              schedules.map((schedule) => (
+                <tr key={schedule.day}>
+                  <td className="day">{schedule.day}</td>
+                  <TdWeek className="week" week={schedule.week}>
+                    {schedule.week}
+                  </TdWeek>
+                  <td className="detail">{schedule.detail}</td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      ) : (
+        <Table>
+          <tbody>
+            {schedules &&
+              schedules.length > 0 &&
+              schedules.map((schedule, index) => (
+                <tr key={index}>
+                  <td className="day">
+                    <input type="text" defaultValue={schedule.day} onBlur={(e) => { handleBlur(e, index, "day") }} />
+                  </td>
+                  <TdWeek className="week" week={schedule.week}>
+                    <input type="text" defaultValue={schedule.week} onBlur={(e) => { handleBlur(e, index, "week") }} />
+                  </TdWeek>
+                  <td className="detail">
+                    <input type="text" defaultValue={schedule.detail} onBlur={(e) => { handleBlur(e, index, "detail") }} />
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      )}
+
     </Container>
   );
 };
@@ -110,6 +140,19 @@ const Table = styled.table`
     width: 70%;
     padding-left: 5px;
     text-align: left;
+  }
+  & td input {
+    width: 60%;
+    align-items: center;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #495057;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: 0.25rem;
+    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
   }
 `;
 
--- src/components/Dashboard/07/Reserve/index.tsx
+++ src/components/Dashboard/07/Reserve/index.tsx
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Title from "../Title";
 import Schedule from "./Schedule";
+import { ReactComponent as ColseImage } from "./../../../../assets/images/close_white.svg";
+import { ReactComponent as SetImage } from "./../../../../assets/images/setting.svg";
+
 
 const Reserve: React.FC = () => {
+  const [editStatus, setEditStatus] = useState(false)
   return (
     <CircleContainer>
+      <EditIconBlock>
+        {editStatus && <ColseImage width={"100%"} height={"100%"} onClick={() => setEditStatus(!editStatus)} />}
+        {!editStatus && <SetImage width={"100%"} height={"100%"} onClick={() => setEditStatus(!editStatus)} />}
+      </EditIconBlock>
       <TitleContent>
         <Title text={"今週の作業予定"} />
       </TitleContent>
       <ScheduleContent>
-        <Schedule />
+        <Schedule editStatus={editStatus} />
       </ScheduleContent>
     </CircleContainer>
   );
 };
 
 const CircleContainer = styled.div`
+  position: relative;
   width: 100%;
   height: 100%;
 `;
@@ -32,5 +41,13 @@ const ScheduleContent = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
+const EditIconBlock = styled.div`
+  position: absolute;
+  width: 25px;
+  height: 25px;
+  top: 0;
+  right: 0;
+`
 
 export default Reserve;
