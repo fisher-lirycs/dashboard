99e3d2e5eb6b62209b01047938dd98ed774b1571
yuqiuliang <yuqiuliang@linkstec.com>
2024-02-19 12:20:25 +0800

    up

--- src/components/Dashboard/07/index.tsx
+++ src/components/Dashboard/07/index.tsx
@@ -17,9 +17,6 @@ import { Link } from "react-router-dom";
 import { LayoutType } from "../../../types/Types";
 
 const Dashboard07: React.FC = () => {
-  console.log("----------------------");
-  console.log(window.innerHeight);
-  console.log("----------------------");
   const [screenHeight] = useState((window.innerHeight < 600 ? 600 : window.innerHeight) - 120);
   const [screenWidth] = useState(window.innerWidth - 30);
   const [sliderStatus, setSliderStatus] = useState(false);
